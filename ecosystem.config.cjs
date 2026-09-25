e45a4c6705e45787fd773d850f129d39254af66e
Jude <developer.mode111@gmail.com>
2026-09-25 05:31:49 +0000

    Fix deploy: stop clobbering .env, use npm ci, pm2 startOrReload + save, pin fork mode

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
     args: 'start -- -p 3014',
     cwd: '/home/projects/matchpoint',
     instances: 1,
+    exec_mode: 'fork',
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
